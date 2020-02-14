91d894fd0e40bfeb76cdbf0951ff8f3dcbf45c85
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2020-02-14 09:31:07 -0500

    Add and configure djangorestframework.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',
+
     # Local
     'posts.apps.PostsConfig',
 ]
@@ -102,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
